2cf19c97823c9c627d8a816d0cdd77564ce01d48
besil <s.bernardinello@reply.it>
2015-01-13 19:02:13 +0100

    Refactoring

--- prototype/script.js
+++ prototype/script.js
@@ -1,15 +1,3 @@
-//function person(first, last, age, eye) {
-//    this.firstName = first;
-//    this.lastName = last;
-//    this.age = age;
-//    this.eyeColor = eye;
-//}
-//person.prototype.nationality = "English";
-//
-//var myFather = new person("John", "Doe", 50, "blue");
-//document.getElementById("demo").innerHTML =
-//"My father is " + myFather.nationality; 
-
 function getUl(f) {
 	var ul = document.createElement("ul");
 	
