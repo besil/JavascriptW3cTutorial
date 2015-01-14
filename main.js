65f31a50b0087a07a025190e57f5f67a0c5adff9
besil <s.bernardinello@reply.it>
2015-01-14 14:45:09 +0100

    Starting bootstrap tutorial

--- main.js
+++ main.js
@@ -11,13 +11,19 @@ function addPage(ref) {
 var pagelist = document.getElementById("pagelist");
 var ul = document.createElement("ul");
 
+var refs = ["prototype", "jquery", "bootstrap"];
+
 var pages = [];
+refs.forEach(function(ref) {
+	pages.push( addPage(ref) );
+});
 
-var prototype = addPage("prototype");
-var jquery = addPage("jquery");
-
-pages.push( prototype );
-pages.push( jquery );
+//var prototype 	= addPage("prototype");
+//var jquery 		= addPage("jquery");
+//var bootstrap 	= addPage("bootstrap");
+//
+//pages.push( prototype );
+//pages.push( jquery );
 
 pages.forEach(function(page) {
 	ul.appendChild( page );
